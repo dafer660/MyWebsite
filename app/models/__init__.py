dd3faa4233e4214482efe4a3dea763e383ddc2fc
Daniel F <danielf18@hotmail.com>
2021-05-05 11:10:44 +0100

    new commit

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# To migrate the DB:
-# 1. flask db init
-# 2. flask db stamp head
-# 3. flask db migrate -m "my description here"
-# 4. flask db upgrade
--- app/static/js/redirect.js
+++
@@ -1,6 +0,0 @@
-window.setTimeout(function(){
-    let protocol = window.location.protocol
-    let host = window.location.host
-    let fullURL = protocol + '//' + host
-    window.location.href = fullURL;
-}, 5000);
